2d94c160a97b4812f0b1e9164f36d6f90d56c003
sruthu-05 <sruthi4689@gmail.com>
2025-08-02 12:09:48 +0530

    loops

--- data_types_05/data_types/loop_otp.task.py
+++ data_types_05/data_types/loop_otp.task.py
@@ -9,12 +9,12 @@ attempts = 3
 
 while attempts:
     user_otp = int(input("Enter the otp:"))
-    if len(str(user_otp)) != 4:
-        print("Invalid OTP. Please enter a 4-digit number.")
+    if len(str(user_otp))!= 4:
+        print("Please enter a 4-digit number")
         if user_otp == otp:
             print("access granted")
             break 
         attempts-=1
     else:
-        print("invalid otp please try again")
+         print("invalid otp please try again")
         
