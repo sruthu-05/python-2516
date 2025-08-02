4475ffb37a426cb817cfe250ec9202e9d1777f48
sruthu-05 <sruthi4689@gmail.com>
2025-08-02 12:10:03 +0530

    student grade

---
+++ data_types_05/data_types/student_grade.task.py
@@ -0,0 +1,6 @@
+student_id = input("Enter student id:")
+student_name = input("Enter student name:")
+attendence = int(input("Enter attendance percentage:"))
+total_score = int(input("Enter total score:"))
+subject_number = int(input("Enter subject number:"))
+
